21ffcf2df3bae6d520474ce1a647232a265244ad
Forhad <developerforhad2@gmail.com>
2024-07-30 13:15:38 +0600

    added to resume section add certificate button to chart

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,3 +1,4 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Helmet } from "react-helmet-async";
 
@@ -8,7 +9,31 @@ const About = () => {
        <title>Forhad Hossain | About me</title>
      </Helmet>
      <ScrollArea className="h-[90vh] py-4 lg:py-6">
-       <h1 className="lg:text-xl text-lg tracking-tight lg:tracking-widest bg-gradient-to-r from-red-500 to-blue-700 inline-block text-transparent bg-clip-text font-medium ">
+       <Accordion type="single" collapsible className="w-1/2 mx-auto">
+         <AccordionItem value="item-1">
+           <AccordionTrigger>
+             <h1 className="text-lg tracking-tight lg:tracking-widest bg-gradient-to-r from-red-500 to-blue-700 inline-block text-transparent bg-clip-text font-medium ">
+               {' '}
+               About,
+             </h1>{' '}
+           </AccordionTrigger>
+           <AccordionContent>
+             <h1 className="lg:text-xl text-lg tracking-tight lg:tracking-widest bg-gradient-to-r from-red-500 to-blue-700 inline-block text-transparent bg-clip-text font-medium ">
+               {' '}
+               I develop Solutions that Empower.
+             </h1>
+             <p className="font-mono text-sm lg:tracking-widest tracking-tight py-2 text-black">
+               I'm Forhad Hossain, a skilled MERN stack developer. With MongoDB,
+               Express.js, React, and Node.js, I've crafted dynamic web
+               applications that captivate. From sleek interfaces to powerful
+               backend systems, I deliver unique online experiences. Let's turn
+               your ideas into exceptional digital realities.
+             </p>{' '}
+           </AccordionContent>
+         </AccordionItem>
+       </Accordion>
+
+       <h1 className="lg:text-xl mt-5 text-lg tracking-tight lg:tracking-widest bg-gradient-to-r from-red-500 to-blue-700 uppercase inline-block text-transparent bg-clip-text font-medium ">
          {' '}
          Hello Everyone,
        </h1>
@@ -22,7 +47,7 @@ const About = () => {
          these technologies.
        </p>
 
-       <h1 className="lg:text-xl text-lg tracking-tight lg:tracking-widest bg-gradient-to-r from-red-500 to-blue-700 inline-block text-transparent bg-clip-text font-medium ">
+       <h1 className="lg:text-xl uppercase text-lg tracking-tight lg:tracking-widest bg-gradient-to-r from-red-500 to-blue-700 inline-block text-transparent bg-clip-text font-medium ">
          What I Do:
        </h1>
        <p className="font-mono text-sm lg:tracking-widest tracking-tight py-2 text-black">
@@ -31,7 +56,7 @@ const About = () => {
          Firebase, and MongoDB. Currently, I am in the process of learning new
          programming languages. I am passionate about programming.
        </p>
-       <h1 className="lg:text-xl text-lg tracking-tight lg:tracking-widest bg-gradient-to-r from-red-500 to-blue-700 inline-block text-transparent bg-clip-text font-medium ">
+       <h1 className="lg:text-xl uppercase text-lg tracking-tight lg:tracking-widest bg-gradient-to-r from-red-500 to-blue-700 inline-block text-transparent bg-clip-text font-medium ">
          Why I Do It:
        </h1>
        <p className="font-mono text-sm lg:tracking-widest tracking-tight py-2 text-black">
@@ -40,7 +65,7 @@ const About = () => {
          new technologies and programming languages and to develop advanced web
          applications.
        </p>
-       <h1 className="lg:text-xl text-lg tracking-tight lg:tracking-widest bg-gradient-to-r from-red-500 to-blue-700 inline-block text-transparent bg-clip-text font-medium ">
+       <h1 className="lg:text-xl text-lg uppercase tracking-tight lg:tracking-widest bg-gradient-to-r from-red-500 to-blue-700 inline-block text-transparent bg-clip-text font-medium ">
          How I Add Value:
        </h1>
        <p className="font-mono text-sm lg:tracking-widest tracking-tight py-2 text-black">
@@ -48,27 +73,27 @@ const About = () => {
          learning new programming languages to create efficient and innovative
          web applications. My approach involves:
        </p>
-       <h1 className="lg:text-xl text-lg tracking-tight lg:tracking-widest bg-gradient-to-r from-red-500 to-blue-700 inline-block text-transparent bg-clip-text font-medium ">
+       <h1 className="lg:text-xl uppercase text-lg tracking-tight lg:tracking-widest bg-gradient-to-r from-red-500 to-blue-700 inline-block text-transparent bg-clip-text font-medium ">
          Problem Solving:
        </h1>
        <p className="font-mono text-sm lg:tracking-widest tracking-tight py-2 text-black">
          Identifying and addressing societal issues through technological
          solutions.
        </p>
-       <h1 className="lg:text-xl text-lg tracking-tight lg:tracking-widest bg-gradient-to-r from-red-500 to-blue-700 inline-block text-transparent bg-clip-text font-medium ">
+       <h1 className="lg:text-xl uppercase text-lg tracking-tight lg:tracking-widest bg-gradient-to-r from-red-500 to-blue-700 inline-block text-transparent bg-clip-text font-medium ">
          Efficiency:
        </h1>
        <p className="font-mono text-sm lg:tracking-widest tracking-tight py-2 text-black">
          Developing user-friendly, efficient, and scalable web applications.
        </p>
-       <h1 className="lg:text-xl text-lg tracking-tight lg:tracking-widest bg-gradient-to-r from-red-500 to-blue-700 inline-block text-transparent bg-clip-text font-medium ">
+       <h1 className="lg:text-xl uppercase text-lg tracking-tight lg:tracking-widest bg-gradient-to-r from-red-500 to-blue-700 inline-block text-transparent bg-clip-text font-medium ">
          Continuous Learning:
        </h1>
        <p className="font-mono text-sm lg:tracking-widest tracking-tight py-2 text-black">
          Staying updated with the latest technologies and programming languages
          to enhance my skill set.
        </p>
-       <h1 className="lg:text-xl text-lg tracking-tight lg:tracking-widest bg-gradient-to-r from-red-500 to-blue-700 inline-block text-transparent bg-clip-text font-medium ">
+       <h1 className="lg:text-xl uppercase text-lg tracking-tight lg:tracking-widest bg-gradient-to-r from-red-500 to-blue-700 inline-block text-transparent bg-clip-text font-medium ">
          Passion for Programming:
        </h1>
        <p className="font-mono text-sm lg:tracking-widest tracking-tight py-2 text-black">
--- src/pages/resume/index.jsx
+++ src/pages/resume/index.jsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import certificate from "../../assets/certificate.png"
 
 const Resume = () => {
  return (
-   <div>
+   <div className="flex flex-col justify-between h-full">
+     <div>  
      <Tabs defaultValue="biography">
        <TabsList className="flex items-center justify-around gap-2">
          <TabsTrigger
@@ -28,22 +32,32 @@ const Resume = () => {
          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 h-full">
            <div className="border">
              <div className="flex items-center border justify-between py-4 px-3">
-               <h1 className="text-lg font-medium">Name</h1>
-               <h1 className="text-lg font-medium">Md Forhad Hossain Babu</h1>
+               <h1 className="font-medium">Name</h1>
+               <h1 className="text-sm font-medium bg-gradient-to-r from-rose-700 to-cyan-700 inline-block text-transparent bg-clip-text">
+                 Md Forhad Hossain Babu
+               </h1>
              </div>
              <div className="flex items-center border justify-between py-4 px-3">
-               <h1 className="text-lg font-medium">Age</h1>
-               <h1 className="text-lg font-medium">17</h1>
+               <h1 className="font-medium">Age</h1>
+               <h1 className="text-sm font-medium">17</h1>
              </div>
              <div className="flex items-center border justify-between py-4 px-3">
-               <h1 className="text-lg font-medium">Age</h1>
-               <h1 className="text-lg font-medium">17</h1>
+               <h1 className="font-medium">Email</h1>
+               <h1 className="text-sm font-medium">
+                 developerforhad2@gmail.com
+               </h1>
              </div>
              <div className="flex items-center border justify-between py-4 px-3">
-               <h1 className="text-lg font-medium">Name</h1>
-               <h1 className="text-lg font-medium">Md Forhad Hossain Babu</h1>
+               <h1 className="font-medium">Linkedin</h1>
+               <h1 className="text-sm font-medium">
+                 <a
+                   className="text-blue-500 hover:underline visited:bg-yellow-400 transition-all duration-300" target="_blank"
+                   href="https://www.linkedin.com/in/mdforhadhossainbabu/"
+                 >
+                   Linkedin
+                 </a>
+               </h1>
              </div>
-            
            </div>
 
            <div className="border">
@@ -65,6 +79,32 @@ const Resume = () => {
        <TabsContent value="Skill">Hello world this Skill</TabsContent>
        <TabsContent value="Education">Hello world this Education</TabsContent>
      </Tabs>
+     </div>
+     <div className="pb-12">    
+     <Dialog>
+       <DialogTrigger asChild>
+         <Button variant="secondary" className="font-bold uppercase hover:border hover:border-blue-400" >Certificate Here</Button>
+       </DialogTrigger>
+       <DialogContent className="sm:max-w-[425px]">
+         <DialogHeader>
+           <DialogTitle>My Achievement</DialogTitle>
+           <DialogDescription>
+             It's long time to my achievement.
+           </DialogDescription>
+         </DialogHeader>
+         <div className="grid gap-4 py-4">
+           <img src={certificate} alt="" />
+         </div>
+         <DialogFooter>
+           <Button>
+             <a href={certificate} download={certificate}>
+               Download Now
+             </a>
+           </Button>
+         </DialogFooter>
+       </DialogContent>
+     </Dialog>
+</div>
    </div>
  );
 };
